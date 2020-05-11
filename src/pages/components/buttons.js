2724fb19d4894f39406b2174ff32ffc640d052cd
Ivan Zeljkovic <ivan.r.zeljkovic@gmail.com>
2020-05-11 13:56:46 +0200

    Add code examples in buttons

--- src/pages/components/buttons.js
+++ src/pages/components/buttons.js
@@ -12,16 +12,50 @@ export default withLayout(function Buttons() {
       <LinkedHeading h="1" id="buttons">Buttons</LinkedHeading>  
       <p className="lead">Custom button styles for actions in forms, dialogs, and more with support for multiple sizes, states, and more.</p>
 
-      <LinkedHeading h="2" id="types">Button types</LinkedHeading>
+      <LinkedHeading h="2" id="button-types">Button types</LinkedHeading>
       <Button className="mb-4" variant="primary">Primary</Button>
       <Button className="mb-4" variant="secondary">Secondary</Button>
       <Button className="mb-4" variant="success">Success</Button>
       <Button className="mb-4" variant="danger">Danger</Button>
-      <Button className="mb-4" ariant="info">Info</Button>
+      <Button className="mb-4" variant="info">Info</Button>
       <Button className="mb-4" variant="warning">Warning</Button>
       <Button className="mb-4" variant="light">Light</Button>
       <Button className="mb-4" variant="dark">Dark</Button>
       <Button className="mb-4" variant="link">Link</Button>
+
+      <LinkedHeading h="3" id="button-types-markup">Button types markup</LinkedHeading>
+     
+      <Row fluid="md" >
+        <Col xl={10} md={12}>
+          <code className="code-snippet">
+            {'<button type="button" class="btn btn-primary">Primary</button>'}
+            <br/>
+            <br/>
+            {'<button type="button" class="btn btn-secondary">Secondary</button>'}
+            <br/>
+            <br/>
+            {'<button type="button" class="btn btn-success">Success</button>'}
+            <br/>
+            <br/>
+            {'<button type="button" class="btn btn-danger">Danger</button>'}
+            <br/>
+            <br/>
+            {'<button type="button" class="btn btn-info">Info</button>'}
+            <br/>
+            <br/>
+            {'<button type="button" class="btn btn-warning">Warning</button>'}
+            <br/>
+            <br/>
+            {'<button type="button" class="btn btn-light">Light</button>'}
+            <br/>
+            <br/>
+            {'<button type="button" class="btn btn-dark">Dark</button>'}
+            <br/>
+            <br/>
+            {'<button type="button" class="btn btn-link">Link</button>'}
+          </code>
+        </Col>
+      </Row>
 
       <LinkedHeading h="2" id="outline-types">Outline buttons</LinkedHeading>
       <p>For a lighter touch, Buttons also come in outline-* variants with no background color.</p>
@@ -35,7 +69,42 @@ export default withLayout(function Buttons() {
       <Button className="mb-4" variant="outline-light">Light</Button>
       <Button className="mb-4" variant="outline-dark">Dark</Button>
 
+      <LinkedHeading h="3" id="outline-button-types-markup">Outline button types markup</LinkedHeading>
+
+      <Row fluid="md" >
+        <Col xl={10} md={12}>
+        <code className="code-snippet">
+          {'<button type="button" class="btn btn-outline-primary">Primary</button>'}
+          <br/>
+          <br/>
+          {'<button type="button" class="btn btn-outline-secondary">Secondary</button>'}
+          <br/>
+          <br/>
+          {'<button type="button" class="btn btn-outline-success">Success</button>'}
+          <br/>
+          <br/>
+          {'<button type="button" class="btn btn-outline-danger">Danger</button>'}
+          <br/>
+          <br/>
+          {'<button type="button" class="btn btn-outline-primary">Info</button>'}
+          <br/>
+          <br/>
+          {'<button type="button" class="btn btn-outline-warning">Warning</button>'}
+          <br/>
+          <br/>
+          {'<button type="button" class="btn btn-outline-light">Light</button>'}
+          <br/>
+          <br/>
+          {'<button type="button" class="btn btn-outline-dark">Dark</button>'}
+          <br/>
+          <br/>
+          {'<button type="button" class="btn btn-outline-link">Link</button>'}
+        </code>
+        </Col>
+      </Row>
+
       <LinkedHeading h="2" id="sizes">Sizes</LinkedHeading>
+
       <p>Fancy larger or smaller buttons? Add <code>size="lg"</code>, <code>size="sm"</code> for additional sizes.</p>
       <div className="mb-2">
         <Button className="mb-4" variant="primary" size="sm">
@@ -56,13 +125,31 @@ export default withLayout(function Buttons() {
       </div>
       
       <div className="mb-2">
-        <Button className="mb-4" variant="primary" size="lg">
+        <Button className="mb-2" variant="primary" size="lg">
           Large button
         </Button>
-        <Button className="mb-4" variant="secondary" size="lg">
+        <Button className="mb-2" variant="secondary" size="lg">
           Large button
         </Button>
       </div>
+
+      <LinkedHeading h="3" id="button-sizes-markup">Sizes markup</LinkedHeading>
+
+      <Row fluid="md" >
+        <Col xl={10} md={12}>
+        <code className="code-snippet">
+          {'<button type="button" class="btn btn-primary btn-sm">Small button</button>'}
+          <br/>
+          <br/>
+          {'<button type="button" class="btn btn-primary">Default button</button>'}
+          <br/>
+          <br/>
+          {'<button type="button" class="btn btn-primary btn-lg">Large button</button>'}
+        </code>
+        </Col>
+      </Row>
+
+      <LinkedHeading h="2" id="block-button">Block Level Button</LinkedHeading>
 
       <p>Create block level buttons—those that span the full width of a parent—by adding <code>block</code></p>
       
@@ -73,9 +160,24 @@ export default withLayout(function Buttons() {
         Block level button
       </Button>
 
+      <LinkedHeading h="3" id="block-button-markup">Block level button markup</LinkedHeading>
+
+      <Row fluid="md" >
+        <Col xl={10} md={12}>
+        <code className="code-snippet">
+          {'<button type="button" class="mb-4 btn btn-primary btn-block btn-lg">Block level button</button>'}
+          <br/>
+          <br/>
+          {'<button type="button" class="mb-4 btn btn-secondary btn-block btn-lg">Block level button</button>'}         
+        </code>
+        </Col>
+      </Row>
+
+      <LinkedHeading h="2" id="button-states">Button states</LinkedHeading>
+
       <Row>
         <Col sm={6}>
-          <LinkedHeading h="2" id="active-button-state">Active State</LinkedHeading>
+          <LinkedHeading h="3" id="active-button-state">Active State</LinkedHeading>
           <Button className="mb-4" variant="primary">
             Primary button
           </Button>
@@ -85,13 +187,35 @@ export default withLayout(function Buttons() {
         </Col>
 
         <Col sm={6}>
-          <LinkedHeading h="2" id="disabled-button-state">Disabled State</LinkedHeading>
+          <LinkedHeading h="3" id="disabled-button-state">Disabled State</LinkedHeading>
           <Button className="mb-4" variant="primary" disabled>
             Primary button
           </Button>
           <Button className="mb-4" variant="secondary" disabled>
             Secondary button
           </Button>
+        </Col>
+      </Row>
+
+      <LinkedHeading h="3" id="button-states-markup">Button states markup</LinkedHeading>
+
+      <Row fluid="md" >
+        <Col xl={10} md={12}>
+        <code className="code-snippet">
+          
+          {'<button type="button" class="mb-4 btn btn-primary">Primary button</button>'}
+          <br/>
+          <br/>
+          {'<button type="button" class="mb-4 btn btn-secondary">Secondary button</button>'} 
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          {'<button disabled type="button" class="mb-4 btn btn-primary">Primary button</button>'}         
+          <br/>
+          <br/>
+          {'<button disabled type="button" class="mb-4 btn btn-secondary">Secondary button</button>'}         
+        </code>
         </Col>
       </Row>
 
