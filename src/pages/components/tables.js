50c62894a1f9f5da869828d32c1000af88052470
Ivan Zeljkovic <ivan.r.zeljkovic@gmail.com>
2020-05-15 16:01:15 +0200

    Add Tables styles

--- src/pages/components/tables.js
+++ src/pages/components/tables.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table , Form, Badge, Dropdown, DropdownButton, InputGroup, Button, FormControl, Col, Row } from 'react-bootstrap';
+import { Table , Form, Badge} from 'react-bootstrap';
 
 import LinkedHeading from '../../components/LinkedHeading';
 import withLayout from "../../with-layout"
@@ -11,39 +11,14 @@ export default withLayout(function Tables() {
         <LinkedHeading h="1" id="tables">Tables</LinkedHeading>   
         <p className="lead">Use the striped, bordered and hover props to customise the table.</p>
         
+        <LinkedHeading h="2" id="simple-table">Simple table</LinkedHeading>   
         
 
         <Table hover responsive className="ebf-table">
             <thead>
                 <tr>
                     <th colSpan="5">
-                        <Row>
-                            <Col> 
-                                <h5>Table Header</h5>
-                                <span>Table Subheader</span>                               
-                            </Col>
-                            <Col>
-                                <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-                                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                                </DropdownButton>
-                            </Col>
-
-                            <Col>
-                                <InputGroup id="search" className="nav-search">          
-                                    <FormControl
-                                        placeholder="Search"
-                                        aria-label="Search"
-                                        aria-describedby="basic-addon"
-                                        type="text"
-                                    />
-                                    <InputGroup.Append>
-                                        <Button type="submit" id="basic-addon"><i className="ebf ebf-search-l"></i></Button>
-                                    </InputGroup.Append>
-                                </InputGroup>
-                            </Col>
-                        </Row>
+                        <h5 className="mb-0 mt-0">Table Header</h5>
                     </th>
                 </tr>
                 <tr>
