f91c325fb73e9ed69d18849c6957a4bdb644c3de
Vladimir Spasic <vladimir.spasic.86@gmail.com>
2020-05-21 23:47:36 +0200

    Added ebf-design-system path prefix for github pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/ebf-design-system",
   siteMetadata: {
     title: `EBF Design system`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
