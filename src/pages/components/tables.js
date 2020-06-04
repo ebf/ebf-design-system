e71234d681b9e880faddd6afc88b07f39d18c9de
Ivan Zeljkovic <ivan.r.zeljkovic@gmail.com>
2020-06-04 14:39:51 +0200

    Add xl table

--- src/pages/components/tables.js
+++ src/pages/components/tables.js
@@ -4,6 +4,14 @@ import { Table , Form, Badge, Button} from 'react-bootstrap';
 import LinkedHeading from '../../components/LinkedHeading';
 import withLayout from "../../with-layout"
 
+function Checkbox({ children, ...props }) {
+    return (
+      <Form.Check type='checkbox' {...props}>
+        {children}
+      </Form.Check>
+    );
+  }
+
 export default withLayout(function Tables() {
     return (
       <>
@@ -23,8 +31,10 @@ export default withLayout(function Tables() {
                     <thead>
                         <tr>
                             <th>
-                                <Form.Group controlId="formBasicCheckbox" className="mb-0">
-                                    <Form.Check type="checkbox"/>
+                                <Form.Group controlId="formBasicCheckbox1" className="mb-0">
+                                    <Form inline>
+                                        <Checkbox id="formBasicCheckbox1" label="1"/>
+                                    </Form>
                                 </Form.Group>
                             </th>
                             <th>Besitzer <i className="ebf ebf-arr-up-fill"></i></th>
@@ -36,8 +46,10 @@ export default withLayout(function Tables() {
                     <tbody>
                         <tr>
                             <td>
-                                <Form.Group controlId="formBasicCheckbox" className="mb-0">
-                                    <Form.Check type="checkbox"/>
+                                <Form.Group controlId="formBasicCheckbox2" className="mb-0">
+                                    <Form inline>
+                                        <Checkbox id="formBasicCheckbox2" label="1"/>
+                                    </Form>
                                 </Form.Group>
                             </td>
                             <td>Mark</td>
@@ -47,8 +59,10 @@ export default withLayout(function Tables() {
                         </tr>
                         <tr>
                             <td>
-                                <Form.Group controlId="formBasicCheckbox" className="mb-0">
-                                    <Form.Check type="checkbox"/>
+                                <Form.Group controlId="formBasicCheckbox3" className="mb-0">
+                                    <Form inline>
+                                        <Checkbox id="formBasicCheckbox3" label="1"/>
+                                    </Form>
                                 </Form.Group>
                             </td>
                             <td>Jacob</td>
@@ -58,8 +72,10 @@ export default withLayout(function Tables() {
                         </tr>
                         <tr>
                             <td>
-                                <Form.Group controlId="formBasicCheckbox" className="mb-0">
-                                    <Form.Check type="checkbox"/>
+                                <Form.Group controlId="formBasicCheckbox4" className="mb-0">
+                                    <Form inline>
+                                        <Checkbox id="formBasicCheckbox4" label="1"/>
+                                    </Form>
                                 </Form.Group>
                             </td>
                             <td>Larry the Bird</td>
@@ -74,6 +90,75 @@ export default withLayout(function Tables() {
             </div>
         </div>
 
+        <LinkedHeading h="2" id="large-table">Large Table</LinkedHeading>
+
+        <div className="ebf-table ebf-table-xl" >
+            <header className="ebf-table-header">
+                <span className="ebf-table-headline">Table Header</span>
+            </header>
+            <div className="ebf-table-contents">
+                <Table hover responsive>
+                    <thead>
+                        <tr>
+                            <th>
+                                <Form.Group controlId="formBasicCheckbox5" className="mb-0">
+                                    <Form inline>
+                                        <Checkbox id="formBasicCheckbox5" label="1"/>
+                                    </Form>
+                                </Form.Group>
+                            </th>
+                            <th>Besitzer <i className="ebf ebf-arr-up-fill"></i></th>
+                            <th>eMail <i className="ebf ebf-arr-up-fill"></i></th>
+                            <th>IMEI <i className="ebf ebf-arr-up-fill"></i></th>
+                            <th>EID <i className="ebf ebf-arr-up-fill"></i></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <Form.Group controlId="formBasicCheckbox6" className="mb-0">
+                                    <Form inline>
+                                        <Checkbox id="formBasicCheckbox6" label="1"/>
+                                    </Form>
+                                </Form.Group>
+                            </td>
+                            <td>Mark</td>
+                            <td>alayaward@ebf.com</td>
+                            <td>54321987</td>
+                            <td><Badge variant="info" className="badge-pill">in Bearbeitung</Badge></td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <Form.Group controlId="formBasicCheckbox7" className="mb-0">
+                                    <Form inline>
+                                        <Checkbox id="formBasicCheckbox7" label="1"/>
+                                    </Form>
+                                </Form.Group>
+                            </td>
+                            <td>Jacob</td>
+                            <td>alayaward@ebf.com</td>
+                            <td>54321987</td>
+                            <td><Badge variant="primary" className="badge-pill">Unbekannt</Badge></td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <Form.Group controlId="formBasicCheckbox8" className="mb-0">
+                                    <Form inline>
+                                        <Checkbox id="formBasicCheckbox8" label="1"/>
+                                    </Form>
+                                </Form.Group>
+                            </td>
+                            <td>Larry the Bird</td>
+                            <td>alayaward@ebf.com</td>
+                            <td>54321987</td>
+                            <td>
+                                <Button className="mb-0" variant="primary" size="sm" >Primary button</Button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
+        </div>
         
       </>
     )
