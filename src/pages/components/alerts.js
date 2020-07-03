9d90f0e7cc6cc8974627b1fac9d220e6b4a9deda
Ivan Zeljkovic <ivan.r.zeljkovic@gmail.com>
2020-07-03 12:44:59 +0200

    Add Alerts Code Snippets

--- src/pages/components/alerts.js
+++ src/pages/components/alerts.js
@@ -29,23 +29,41 @@ export default withLayout(function Alerts () {
         Alerts are available for any length of text, as well as an optional dismiss button.
         For proper styling, use one of the eight <code>variant</code>s.
       </p>
-      
-      {ALERT_TYPES.map((value, idx) => (
+
+
+       {ALERT_TYPES.map((value, idx) => (
         <Alert key={idx} variant={value.type}>
           {/* TODO */}
           <i className={'alert-icon ebf ' + value.icon}></i>
-          This is a {value.type} alert — check it out!
+          This is a {value.type} alert — check it out! .
         </Alert>
-      ))}
+      ))}      
+      
 
-      <Row fluid="md" >
-        <Col xl={10} md={12}>
-        <code className="code-snippet">
-          {'<div role="alert" class="fade alert alert-primary show"><i class="alert-icon ebf ebf-bell-fill"></i> This is a primary alert — check it out!</div>'}
-                
-        </code>
-        </Col>
-      </Row>
+      {ALERT_TYPES.map(({ type, icon }) => {
+          const variant = `${type}`;
+
+          return (
+            <div className="mb-3 capitalize-text">
+              <LinkedHeading h="3" id={type + "-alert"} >{type} Alert</LinkedHeading>
+              
+              <Alert  variant={type}>
+                <i className={'alert-icon ebf ' + icon}></i>
+                This is a {type} alert — check it out!
+              </Alert>
+
+              <CopyBlock
+                theme={monoBlue}
+                text={`<div role="alert" class="fade alert alert-${type} show">\n\t<i class="alert-icon ebf ${icon}"></i> This is a ${type} alert - check it out!\n</div>`}
+                language="jsx"
+                showLineNumbers
+                wrapLines
+                codeBlock
+              />
+            </div>
+          )
+        })}
+
 
       <LinkedHeading h="2" id="links">Links</LinkedHeading>
       <p>Use the <code>.alert-link</code> utility class to quickly provide matching colored links within any alert.</p>
@@ -58,37 +76,31 @@ export default withLayout(function Alerts () {
         </Alert>
       ))}
 
-      
+      {ALERT_TYPES.map(({ type, icon }) => {
+        const variant = `${type}`;
 
-<Row fluid="md" >
-        <Col xl={10} md={12}>
-        <code className="code-snippet">
-          {'<div role="alert" class="fade alert alert-primary show"><i class="alert-icon ebf ebf-bell-fill"></i>This is a primary alert with  <a class="alert-link" href="#" role="button">an example link</a>. Give it a click if you like.</div>'}
-          <br/>
-          <br/>
-          {'<div role="alert" class="fade alert alert-secondary show"><i class="alert-icon ebf ebf-bell-fill"></i>This is a secondary alert with  <a class="alert-link" href="#" role="button">an example link</a>. Give it a click if you like.</div>'}       
-          <br/>
-          <br/>
-          {'<div role="alert" class="fade alert alert-success show"><i class="alert-icon ebf ebf-check-mark-circle"></i>This is a success alert with  <a class="alert-link" href="#" role="button">an example link</a>. Give it a click if you like.</div>'}       
-          <br/>
-          <br/>
-          {'<div role="alert" class="fade alert alert-danger show"><i class="alert-icon ebf ebf-alert-wide-fill"></i>This is a danger alert with  <a class="alert-link" href="#" role="button">an example link</a>. Give it a click if you like.</div>'}       
-          <br/>
-          <br/>
-          {'<div role="alert" class="fade alert alert-warning show"><i class="alert-icon ebf ebf-alert-wide-fill"></i>This is a warning alert with  <a class="alert-link" href="#" role="button">an example link</a>. Give it a click if you like.</div>'}       
-          <br/>
-          <br/>
-          {'<div role="alert" class="fade alert alert-info show"><i class="alert-icon ebf ebf-info-fill"></i>This is a info alert with  <a class="alert-link" href="#" role="button">an example link</a>. Give it a click if you like.</div>'}       
-          <br/>
-          <br/>
-          {'<div role="alert" class="fade alert alert-light show"><i class="alert-icon ebf ebf-bell-fill"></i>This is a light alert with  <a class="alert-link" href="#" role="button">an example link</a>. Give it a click if you like.</div>'}       
-          <br/>
-          <br/>
-          {'<div role="alert" class="fade alert alert-dark show"><i class="alert-icon ebf ebf-bell-fill"></i>This is a dark alert with  <a class="alert-link" href="#" role="button">an example link</a>. Give it a click if you like.</div>'}       
-        </code>
-        </Col>
-      </Row>
-      
+        return (
+          <div className="mb-3 capitalize-text">
+            <LinkedHeading h="3" id={type + "-alert"} >{type} Alert</LinkedHeading>
+            
+            <Alert  variant={type}>
+              <i className={'alert-icon ebf ' + icon}></i>
+              This is a {type} alert with{' '} <Alert.Link href="#">an example link</Alert.Link>. Give it a click if you like.
+            </Alert>
+
+            <CopyBlock
+              theme={monoBlue}
+              text={`<div role="alert" class="fade alert alert-${type} show">\n\t<i class="alert-icon ebf ${icon}"></i> \n\t This is a ${type} alert with <a class="alert-link" href="#" role="button">an example link</a>.\n</div>`}
+              language="jsx"
+              showLineNumbers
+              wrapLines
+              codeBlock
+            />
+          </div>
+        )
+      })}      
+
+
     </>
   )
 });
