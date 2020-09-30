3c71e3e3aa9a81ecbb506849193ea013be00a74b
Ivan Zeljkovic <ivan.r.zeljkovic@gmail.com>
2020-09-30 12:45:10 +0200

    Updates

---
+++ react-syntax-highlighter_languages_refractor_ini-f343e68eba9f72461e51.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[84],{"22HQ":function(t,n,i){"use strict";function s(t){t.languages.ini={comment:/^[ \t]*[;#].*$/m,selector:/^[ \t]*\[.*?\]/m,constant:/^[ \t]*[^\s=]+?(?=[ \t]*=)/m,"attr-value":{pattern:/=.*/,inside:{punctuation:/^[=]/}}}}t.exports=s,s.displayName="ini",s.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_ini-f343e68eba9f72461e51.js.map
