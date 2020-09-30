caa4cb8581de668afd72ac46e3346dc0e686b811
Ivan Zeljkovic <ivan.r.zeljkovic@gmail.com>
2020-09-30 13:02:07 +0200

    Updates

---
+++ react-syntax-highlighter_languages_refractor_hsts-4c524297dab0e9aaf2d0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[84],{"3DeZ":function(a,e,s){"use strict";function n(a){a.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}a.exports=n,n.displayName="hsts",n.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts-4c524297dab0e9aaf2d0.js.map
