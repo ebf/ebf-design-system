168ed4df450c9272753794818256c86530348332
Vladimir Spasic <vladimir.spasic.86@gmail.com>
2020-10-05 13:08:06 +0200

    Update Layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,11 @@ module.exports = {
   pathPrefix: "/ebf-design-system",
   siteMetadata: {
     title: `EBF Design system`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `EBF Design system documentation website`,
     author: `@ebf`,
   },
   plugins: [
+    `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-sass`,
@@ -33,17 +34,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `EBF Design System`,
+        short_name: `EBF Design System`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,6 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 import { Container, Row, Col } from 'react-bootstrap';
 import logo from '../images/logo.svg'
 
@@ -16,58 +15,46 @@ import SideNav from "./SideNav"
 import Toc, { TocProvider } from './Toc';
 import "../styles/main.scss";
 
-const Layout = ({ children, location }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+const Layout = ({ children, location }) => (
+  <main className="d-flex flex-column min-vh-100">
+    <Header siteTitle="EBF Design System" />
 
-  return (
-    <main className="d-flex flex-column">
-      <Header siteTitle={data.site.siteMetadata.title} />
+    <Container fluid className="flex-grow-1">
+      <Row>
+        <TocProvider>
+          <Col as={SideNav} xs={12} md={3} xl={2} location={location} />
+          <Col as={Toc} className="d-none d-xl-block" xl={2} />
+          <Col xs={12} md={9} xl={8} id="rb-docs-content">
+            {children}
+          </Col>
+        </TocProvider>
+      </Row>
 
-      <Container fluid className="flex-grow-1">
-        <Row>
-          <TocProvider>
-            <Col as={SideNav} xs={12} md={3} xl={2} location={location} />
-            <Col as={Toc} className="d-none d-xl-block" xl={2} />
-            <Col xs={12} md={9} xl={8} id="rb-docs-content">
-              {children}
-            </Col>
-          </TocProvider>
-        </Row>
+      
+      
+    </Container>
 
-        
-        
-      </Container>
-
-      <footer className="footer shadow doc-footer">
-          <div className="container footer-container">
-              <div className="centering-div">
-                  <span>© {new Date().getFullYear()} <a href="https://www.ebf.com">EBF</a></span>
-              </div>
-              <div className="centering-div">
-                  <img
-                      src={logo}
-                      width="auto"
-                      height="24"
-                      className="d-inline-block align-middle"
-                      alt="Logo"
-                  />
-              </div>
-              <div className="centering-div">
-                <span>v1.0.11</span>
-              </div>                
-          </div>
-        </footer>
-    </main>
-  )
-}
+    <footer className="footer shadow doc-footer">
+        <div className="container footer-container">
+            <div className="centering-div">
+                <span>© {new Date().getFullYear()} <a href="https://www.ebf.com">EBF</a></span>
+            </div>
+            <div className="centering-div">
+                <img
+                    src={logo}
+                    width="auto"
+                    height="24"
+                    className="d-inline-block align-middle"
+                    alt="Logo"
+                />
+            </div>
+            <div className="centering-div">
+              <span>v1.0.10</span>
+            </div>                
+        </div>
+      </footer>
+  </main>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
