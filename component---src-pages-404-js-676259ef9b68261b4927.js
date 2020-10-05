9f582d4a737ba81652be372765c3165b2656258c
Vladimir Spasic <vladimir.spasic.86@gmail.com>
2020-10-05 13:13:19 +0200

    Updates

--- component---src-pages-404-js-676259ef9b68261b4927.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(t,e,n){"use strict";n.r(e);var a=n("q1tI"),u=n.n(a),l=n("Z/SS");e.default=Object(l.a)((function(){return u.a.createElement(u.a.Fragment,null,u.a.createElement("h1",null,"NOT FOUND"),u.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}),{title:"404: Not found"})}}]);
-//# sourceMappingURL=component---src-pages-404-js-676259ef9b68261b4927.js.map
