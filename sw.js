8d3c72bb00d1ebe2eeeef868788bb946176d001a
Ivan Zeljkovic <ivan.r.zeljkovic@gmail.com>
2020-10-07 11:32:41 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3dc170dacb732994beed.js"
+    "url": "webpack-runtime-d4d6032f7621a9ec1c7c.js"
   },
   {
     "url": "framework-7c17e37070ebc39b0fbd.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6e32c6a3488380f96f6efb39413c761e"
+    "revision": "f32c7ec672d754b31d2820c9b76cdd1b"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-6d7462888018cb13a12a.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "585c2ea45f11842eb71c03ab7366ade0"
+    "revision": "30f2e2d4716dab7d805ad46199c825de"
   },
   {
     "url": "polyfill-bd07493e596e2457574f.js"
