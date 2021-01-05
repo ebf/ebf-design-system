d46f4367d51c7ad7770adc3ccabed334eb25eb6a
Vladimir Spasic <vladimir.spasic.86@gmail.com>
2021-01-05 11:39:06 +0100

    Update public tree

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 /* eslint-disable node/no-extraneous-require, node/no-unpublished-require */
 'use strict';
-// const path = require('path');
-// const resolve = require('resolve');
-// const Funnel = require('broccoli-funnel');
-// const mergeTrees = require('broccoli-merge-trees');
+const path = require('path');
+const Funnel = require('broccoli-funnel');
 
 module.exports = {
   name: require('./package').name,
+
+  /**
+   * Override the method in order not to include the addon name when public resources
+   * are processed by the Ember build process. We would like to put the resources in
+   * the root `public` folder in the final build.
+   *
+   * @param {Tree} tree Public tree
+   */
+  treeForPublic() {
+    return new Funnel(path.resolve('./public'), {
+      destDir: '/',
+      annotation: `Addon#treeForPublic (${this.name})`,
+    });
+  },
 };
