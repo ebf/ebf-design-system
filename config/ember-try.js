978ae102538c56e4b7b2de6ea9591ded111232a1
Vladimir Spasic <vladimir.spasic.86@gmail.com>
2021-03-23 22:46:21 +0100

    Remove canary and beta builds

--- config/ember-try.js
+++ config/ember-try.js
@@ -23,22 +23,6 @@ module.exports = async function() {
           }
         }
       },
-      {
-        name: 'ember-beta',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('beta')
-          }
-        }
-      },
-      {
-        name: 'ember-canary',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('canary')
-          }
-        }
-      }
     ]
   };
 };
