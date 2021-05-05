09e228f1a7d7fbd591819e4cbc6422ef71b862ef
Ivan Zeljkovic <ivan.r.zeljkovic@gmail.com>
2021-05-05 14:37:34 +0200

    Tweak Notifications
    Removed ebf- name from classes
    
    Added .nav-link class to .dropdown-toggle-notification since the most common use will be within .navbar-nav container.
    
    DEV TODO: Add contidion for .dropdown-toggle element to append .nav-link when used in .navbar-nav
    
    Hint: @navlink="true"

--- tests/dummy/app/components/docs/notifications/example.js
+++ tests/dummy/app/components/docs/notifications/example.js
@@ -8,7 +8,7 @@ export default class DocsNotificationsExampleComponent extends Component {
   @tracked selected = null;
 
   @tracked notifications = [{
-    icon: 'ebf-grid',
+    icon: 'ebf-eye',
     title: 'Tec-9 Review',
     message: 'Now that there is the Tec-9, a crappy spray gun from South Miami. This gun is advertised as the most popular gun in American crime. Do you believe that shit?',
     timestamp: moment().subtract(17, 'm'),
@@ -18,12 +18,12 @@ export default class DocsNotificationsExampleComponent extends Component {
     message: 'The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men.',
     timestamp: moment().subtract(1, 'h'),
   }, {
-    icon: 'ebf-complain-hammer-fill',
+    icon: 'ebf-complain-hammer',
     title: 'Fact of the day',
     message: 'You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder.',
     timestamp: moment().subtract(3, 'd'),
   }, {
-    icon: 'ebf-apple',
+    icon: 'ebf-device-smartphone',
     title: 'Phone received',
     message: 'Curabitur ipsum est, porttitor vel turpis in, suscipit finibus urna. In laoreet eget nulla et viverra.',
     timestamp: moment().subtract(4, 'd'),
