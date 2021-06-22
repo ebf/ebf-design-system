c2058fdf39aeaab1cb16952195ecac028747a22b
Ivan Zeljkovic <ivan.r.zeljkovic@gmail.com>
2021-06-22 11:48:10 +0200

    Fix Tests

--- tests/integration/components/layout/footer-test.js
+++ tests/integration/components/layout/footer-test.js
@@ -19,20 +19,21 @@ module('Integration | Component | layout/footer', function(hooks) {
 
   test('it renders with fluid container and default columns', async function(assert) {
     await render(hbs`
-      <Layout::Footer
-        @fluid={{true}}
-        @copyright="EBG GmbH"
-        @logo="/ebf-logo.png"
-        @version="1.0.0"
-        class="shadow"
-      />
+    <Layout::Footer
+      @fluid={{true}}
+      @dark={{true}}
+      @copyright="Deutsche Telekom AG"
+      @logo="/telekom-logo-magenta.svg"
+      @label="Deutsche Telekom AG"
+      @version="1.0.0"
+      class="shadow" 
+    />
     `);
-
     assert.dom('.container-fluid').exists();
-    assert.dom('.footer').containsText('EBG GmbH');
+    assert.dom('.footer').containsText('Deutsche Telekom AG');
     assert.dom('.footer').containsText('1.0.0');
-    assert.dom('.footer img').hasAttribute('src', '/ebf-logo.png')
-      .hasAttribute('alt', 'EBG GmbH')
-      .hasAttribute('title', 'EBG GmbH');
+    assert.dom('.footer img').hasAttribute('src', '/telekom-logo-magenta.svg')
+      .hasAttribute('alt', 'Deutsche Telekom AG')
+      .hasAttribute('title', 'Deutsche Telekom AG');
   });
 });
